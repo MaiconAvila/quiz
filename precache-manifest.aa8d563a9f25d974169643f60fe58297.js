21c19959cede2c467a1f691799bc0852eb6dd5ad
Maicon Avila <maiconavila00@gmail.com>
2020-07-01 14:55:31 -0300

    Updates

---
+++ precache-manifest.aa8d563a9f25d974169643f60fe58297.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47def64467b061da48674bb95b6e0012",
+    "url": "/country-quiz/index.html"
+  },
+  {
+    "revision": "71e51b3e2808f9b35cf5",
+    "url": "/country-quiz/static/js/2.9731b8e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/country-quiz/static/js/2.9731b8e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc8ad7c17866d8e8cbcd",
+    "url": "/country-quiz/static/js/main.bab0ff5b.chunk.js"
+  },
+  {
+    "revision": "f97cf84ba37da8c4dd6e",
+    "url": "/country-quiz/static/js/runtime-main.ba5928b2.js"
+  }
+]);
